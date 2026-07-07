f814531db1fddca2fc905490c2c968b4c5a14ae5
anshika660 <anshikasharma15122005@gmail.com>
2026-07-07 15:09:51 +0530

    Day 1 Java Basics

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, TalentxMinds!");
+        System.out.println("My first Java program.");
+    }
+}
